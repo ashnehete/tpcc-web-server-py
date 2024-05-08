e2968e319d37cbe1821248cd5e8be1d0d179e608
Aashish Nehete <ashnehete@gmail.com>
2024-05-08 00:47:21 -0400

    Bugfix in litefs/run.sh

--- litefs/run.sh
+++ litefs/run.sh
@@ -8,7 +8,7 @@ fi
 
 case $1 in
   help)
-    echo "up|down|nginx|primary|replica"
+    echo "up|down|nginx|primary|replica|ps"
     ;;
   nginx)
     docker compose -f litefs/docker-compose.yml up -d nginx
@@ -17,13 +17,16 @@ case $1 in
     docker compose -f litefs/docker-compose.yml up -d primary
     ;;
   replica)
-    docker compose -f litefs/docker-compose.yml up -d nginx
+    docker compose -f litefs/docker-compose.yml up -d replica
     ;;
   up)
     docker compose -f litefs/docker-compose.yml up -d
     ;;
   down)
     docker compose -f litefs/docker-compose.yml down
+    ;;
+  ps)
+    docker compose -f litefs/docker-compose.yml ps
     ;;
   *)
     echo "Invalid argument: '$1'"
